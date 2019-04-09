99ff48df97d162eb42102a865ae307b1cd4f1312
Francis Chen <fncis.chen@gmail.com>
2019-04-09 14:14:52 -0700

    setting up react components and test structure

--- fncischen/src/App.test.js
+++ fncischen/src/App.test.js
@@ -2,8 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
+import { shallow } from 'enzyme';
+import 'jest-dom/extend-expect';
+
+describe('<App/>', () => {
+
+  it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+  it('renders successfully', () => {
+    render(<App/>, div);
+  })
+
+  it("renders Hello World", () => {
+    
+  })
 });
