cebdd360bb1b0ba7912d49a821119c295cb37d84
Francis Chen <fncis.chen@gmail.com>
2019-04-10 16:53:50 -0700

    refactored app-test.js

--- fncischen/src/App.test.js
+++ fncischen/src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Display from "./Components/Display.js";
 
 import { render, cleanup, fireEvent } from 'react-testing-library';
 
@@ -8,18 +9,15 @@ import { render, cleanup, fireEvent } from 'react-testing-library';
 import { shallow } from 'enzyme';
 import 'jest-dom/extend-expect';
 
+afterEach(cleanup);
+
 // https://medium.com/@antonybudianto/react-testing-with-react-testing-library-3b8554f6acfd
 describe('<App/>', () => {
 
-  it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
-
   it('renders successfully', () => {
-    render(<App/>, div);
-  })
+      render(<App/>);
+  });
+  
 
   it('check balls', () => {
     const { getByText } = render(<Display balls="0"/>)
